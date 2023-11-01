c8b70ba41f50a5d9d0e1b6c3ebbe3a806cf13624
bunnana <na.nuttanont@gmail.com>
2023-11-01 16:41:44 +0700

    feat: :sparkles: add Auth route

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import express from "express";
-import { PrismaClient } from "@prisma/client";
 import { PORT } from "./configs";
+import authRouter from "./routers/Auth/Auth.router";
 
-const client = new PrismaClient();
 const app = express();
+app.use(express.json());
+
+app.use("/user", authRouter);
 
 app.listen(PORT, () => console.log(`Server is listening at port ${PORT}`));
