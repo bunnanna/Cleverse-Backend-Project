c680fc1873e7fae031e4a7a530d3e1e238643ed6
bunnana <na.nuttanont@gmail.com>
2023-11-02 12:13:29 +0700

    feat: :sparkles: add content controller

---
+++ src/controllers/Content/Content.controller.ts
@@ -0,0 +1,28 @@
+import { TContentService } from "../../services/Content";
+import { TContentController } from "./Content.controller.type";
+
+export default class ContentController implements TContentController {
+	constructor(private service: TContentService) {}
+	getAll: TContentController["getAll"] = async (req, res) => {
+		const contents = await this.service.getAll();
+		return res.status(200).json(contents);
+	};
+	getOne: TContentController["getOne"] = async (req, res) => {
+		const { id } = req.params;
+		const content = await this.service.getOne(id);
+		return res.status(200).json(content).end();
+	};
+	update: TContentController["update"] = async (req, res) => {
+		const { id } = req.params;
+		const updateBody = req.body;
+		const { user } = res.locals;
+		const content = await this.service.update(id, updateBody, user);
+		return res.status(200).json(content).end();
+	};
+	delete: TContentController["delete"] = async (req, res) => {
+		const { id } = req.params;
+		const { user } = res.locals;
+		const content = await this.service.delete(id, user);
+		return res.status(200).json(content).end();
+	};
+}
---
+++ src/controllers/Content/Content.controller.type.ts
@@ -0,0 +1,24 @@
+import { RequestHandler } from "express";
+import { TContentDTO, TCreateContentDTO, TUpdateContentDTO } from "../../dto";
+import { TContent, TUpdateContent } from "../../types/content";
+import { TLocal } from "../../types/user";
+
+export type TContentController = {
+	getAll: RequestHandler<{}, TContent[]>;
+	getOne: RequestHandler<{ id: string }, TContent>;
+	// create: RequestHandler<{}, TContent, TCreateContentDTO, unknown, TLocal>;
+	update: RequestHandler<
+		{ id: string },
+		TContent,
+		TUpdateContentDTO,
+		unknown,
+		TLocal
+	>;
+	delete: RequestHandler<
+		{ id: string },
+		TContent,
+		TCreateContentDTO,
+		unknown,
+		TLocal
+	>;
+};
